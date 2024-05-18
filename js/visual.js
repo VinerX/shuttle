0eab9e5241934c44f7d7e9aae846db34cad2d672
VinerX <60321176+VinerX@users.noreply.github.com>
2024-05-18 11:09:03 +0300

    Удалил старые наброски передачи данных пользователю

--- js/visual.js
+++ js/visual.js
@@ -7,18 +7,6 @@ var angle = 0;
 var FuelUsage = 0;
 let validFunc = new Function('return[0,0]');
 let funcResult = [0,0];
-// Для чтения пользователю
-//const X = parseInt(inputs[0]);
-//const Y = parseInt(inputs[1]);
-//const hSpeed = parseInt(inputs[2]); // the horizontal speed (in m/s), can be negative.
-//const vSpeed = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
-//const fuel = parseInt(inputs[4]); // the quantity of remaining fuel in liters.
-//const rotate = parseInt(inputs[5]); // the rotation angle in degrees (-90 to 90).
-//const power = parseInt(inputs[6]); // the thrust power (0 to 4).
-
-
-
-
 //Приложение
 var app;
 var height;
